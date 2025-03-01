0def9af3163df5381dea377780c6fec790c744f6
Vaishnavi k <vaishnavik1602@gmail.com>
2025-03-01 17:53:24 +0530

    Added user and appointment routes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
+const userRoutes = require("./routes/userRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+const connectDB = require("./config/db");
 require("dotenv").config();
 const express = require("express");
+const appointment = require("./config/models/appointment");
 console.log("MONGO_URI:", process.env.MONGO_URI);
 console.log("PORT:",process.env.PORT);
 
 const app = express();
 app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/appointments", appointments)
 
 console.log("Starting server...");
 
